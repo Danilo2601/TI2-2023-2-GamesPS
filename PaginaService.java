06ea3e9328961ed9de96f01b2cecc221cef4c9f6
Danilo2601 <81717641+Danilo2601@users.noreply.github.com>
2023-11-22 23:57:31 -0300

    Add files via upload

---
+++ PaginaService.java
@@ -0,0 +1,126 @@
+package service;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import DAO.JogoDAO;
+import model.Jogo;
+import spark.Request;
+import spark.Response;
+
+public class PaginaService {
+	private JogoDAO jogoDAO = new JogoDAO();
+	private String form;
+	
+	public Object preencher(Request request,Response response) {
+		Path resourcePath = Paths.get("src", "main", "resources", "public");
+        
+	    Path filePath = resourcePath.resolve("pag_jogo.html"); 
+	    
+	    int id = Integer.parseInt(request.params(":id"));	
+	    
+	    Jogo jogo = jogoDAO.get(id);
+
+	    String nomeArquivo = ""+filePath;
+		form = "";
+		try{
+			Scanner entrada = new Scanner(new File(nomeArquivo));
+		    while(entrada.hasNext()){
+		    	form += (entrada.nextLine() + "\n");
+		    }
+		    entrada.close();
+		}  catch (Exception e) { System.out.println(e.getMessage()); }
+		
+		String pegarJogo = "<div class=\"carousel-inner \">";
+		
+		pegarJogo += "<div class=\"carousel-item active\"><img src=\""+jogo.getImagens()[0]+"\" class=\"d-block w-100\"alt=\"...\" /></div>";
+		for (int i = 0; i < jogo.getImagens().length; i++) {
+	        pegarJogo += "<div class=\"carousel-item\"><img src=\""+jogo.getImagens()[i]+"\" class=\"d-block w-100\"alt=\"...\" /></div>";
+	    }
+		
+		pegarJogo += "</div>";
+		
+		form = form.replaceFirst("<div class=\"carousel-inner \"></div>", pegarJogo);
+		
+		String pegarJogo2 = "<div class=\"carousel-indicators\">";
+		
+		pegarJogo2 += "<button type=\"button\" data-bs-target=\"#carouselExampleIndicators\" data-bs-slide-to=\"0\" class=\"active 0 previw b S\" aria-current=\"true\" aria-label=\"Slide 1\"> <img class=\"d-block\"src=\""+jogo.getImagens()[0]+"\"class=\"img-fluid\" />";
+		
+		for (int i = 0; i < jogo.getImagens().length; i++) {
+	        pegarJogo += "<button type=\"button\" data-bs-target=\"#carouselExampleIndicators\" data-bs-slide-to=\""+(i+1)+"\" aria-label=\"Slide "+(i+2)+"\" class=\"previw "+i+" b\"> <img class=\"d-block\"src=\""+jogo.getImagens()[i]+"\"class=\"img-fluid\" />";
+	    }
+		
+		pegarJogo2 += "</div>";
+		
+		form = form.replaceFirst("<div class=\"carousel-indicators\"></div>", pegarJogo2);
+		
+		String pegarJogo3 = "<div id=\"info\" class=\"col\">\r\n"
+				+ "                <div id=\"comp\">\r\n"
+				+ "                    <h4 class=\"W\">By:"+jogo.getEmpresa()+"</h4>\r\n"
+				+ "                    <h4 id=\"name\">"+jogo.getNome()+"</h4>\r\n"
+				+ "                    <button class=\"fas fa-star\"id=\"SAVE\"></button>\r\n"
+				+ "                    <div id=\"score\">"+jogo.getAvaliacao()+"</div>\r\n"
+				+ "                </div>\r\n"
+				+ "                <p id=\"sinopsys\">"+jogo.getDescricao()+"</p>\r\n"
+				+ "            </div>";
+		
+		form = form.replaceFirst("<div id=\"info\" class=\"col\"></div>", pegarJogo3);
+		
+		return form;
+	}
+
+	public Object loadHeader(boolean gerenciador, String usuario){
+		form = "";
+		String header = "";
+		if(gerenciador){
+			header += "<button onclick=\"toggleSidebar()\" class=\"btn_icon_header\">"
+			+"				<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" fill=\"currentColor\" class=\"bi bi-list\" viewBox=\"0 0 16 16\">"
+			+"					<path fill-rule=\"evenodd\" d=\"M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z\"/>"
+			+"				</svg>"
+			+"			</button>"
+			+"			<div class=\"logo_header\">"
+			+"				<a href=\"index.html\">"
+			+"					<img src=\"/image/logo-ti2.png\" alt=\"Logo\">"
+			+"				</a>"
+			+"			</div>"
+			+"			<div class=\"navigation_header\" id=\"navigation_header\">"
+			+"				<button onclick=\"toggleSidebar()\" class=\"btn_icon_header\" id=\"btn2\">"
+			+"				<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-x\" viewBox=\"0 0 16 16\">"
+			+"					<path d=\"M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z\"/>"
+			+"				</svg>"
+			+"				</button>"
+			+"				<a href=\"/crud_jogo.html\" >Jogos</a>"
+			+"				<a href=\"/crudUser.html\">Usuários</a>"
+			+"				<a href=\"/pesquisa.html\">Search</a>"
+			+"				<a href=\"/usuario.html\">"+usuario+"</a>"
+			+"			</div>";
+
+		}else{
+			header += "<button onclick=\"toggleSidebar()\" class=\"btn_icon_header\">"
+			+"				<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" fill=\"currentColor\" class=\"bi bi-list\" viewBox=\"0 0 16 16\">"
+			+"					<path fill-rule=\"evenodd\" d=\"M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z\"/>"
+			+"				</svg>"
+			+"			</button>"
+			+"			<div class=\"logo_header\">"
+			+"				<a href=\"index.html\">"
+			+"					<img src=\"/image/logo-ti2.png\" alt=\"Logo\">"
+			+"				</a>"
+			+"			</div>"
+			+"			<div class=\"navigation_header\" id=\"navigation_header\">"
+			+"				<button onclick=\"toggleSidebar()\" class=\"btn_icon_header\" id=\"btn2\">"
+			+"				<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-x\" viewBox=\"0 0 16 16\">"
+			+"					<path d=\"M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z\"/>"
+			+"				</svg>"
+			+"				</button>"
+			+"				<a href=\"/pesquisa.html\">Search</a>"
+			+"				<a href=\"/usuario.html\">"+usuario+"</a>"
+			+"			</div>";
+		}
+
+		form = form.replaceFirst("<HEADER>", header);
+
+		return header;
+	}
+}
---
+++ SearchService.java
@@ -0,0 +1,66 @@
+package service;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+import DAO.JogoDAO;
+import model.Jogo;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+public class SearchService {
+	private JogoDAO jogoDAO = new JogoDAO();
+	private PaginaService paginaService = new PaginaService();
+	private String form;
+	
+	
+	public Object pesquisa(Request request, Response response) {
+		
+		Path resourcePath = Paths.get("src", "main", "resources", "public");
+
+	        
+	    Path filePath = resourcePath.resolve("pesquisa.html"); 
+
+	    String alvo = request.queryParams("searcher");
+	    
+	    
+		
+		String nomeArquivo = ""+filePath;
+		form = "";
+		try{
+			Scanner entrada = new Scanner(new File(nomeArquivo));
+		    while(entrada.hasNext()){
+		    	form += (entrada.nextLine() + "\n");
+		    }
+		    entrada.close();
+		}  catch (Exception e) { System.out.println(e.getMessage()); }
+		
+
+		
+		String pegaJogo = "";
+		List<Jogo> jogos = jogoDAO.getSearch(alvo);
+		
+		for(Jogo p : jogos) 
+		{
+			
+			pegaJogo += "<a class=\"game-card\">\n";
+			pegaJogo += "<img src=\""+ p.getImagens()[0] +"\">";
+			pegaJogo += "<h3>"+ p.getNome() +"</h3>";
+			pegaJogo += "</a>\r\n";
+
+		}
+		
+		Session session = request.session();
+		boolean gerenciador = session.attribute("gerenciador");
+		String usuario = session.attribute("usuario");
+		paginaService.loadHeader(gerenciador, usuario);
+		
+		form = form.replaceFirst("<input type=\"hidden\" id=\"msg\" name=\"msg\" value=\"\">", pegaJogo);
+		
+		return form;
+	}
+}
